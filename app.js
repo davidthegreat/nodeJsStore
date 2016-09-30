600d0678c8637de898831d638ebc6f8276b3a2ba
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-09-30 00:31:35 -0700

    require all the dependecies to signup a user

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars')
-var mongoose = require('mongoose')
+var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
+var userRoutes = require('./routes/user')
 
 var app = express();
 
-mongoose.connect('localhost:27017/shopping')
+mongoose.connect('localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}))
@@ -23,8 +28,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/user', userRoutes)
 app.use('/', routes);
 
 
