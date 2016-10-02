97e69575df1857b67f6a8147e92fd8e7d78cb1e9
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-10-02 01:05:10 -0700

    add a new order variable grab all the information from that order to be saved

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var Product = require('../models/product');
-var Cart = require('../models/cart')
+var Cart = require('../models/cart');
+var Order = require('../models/order');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -70,9 +71,22 @@ router.post('/checkout', function(req, res, next){
       req.flash('error', err.message);
       return res.redirect('/checkout');
      }
-     req.flash('success', 'Successfully bought product');
-     req.session.cart = null;
-     res.redirect('/');
+     var order = new Order({
+        user: req.user,
+        cart: cart,
+        address: req.body.address,
+        name: req.body.name,
+        paymentId: charge.id
+     });
+
+     order.save(function(err, result){
+     if(err){
+      return res.redirect('/checkout');
+     }
+      req.flash('success', 'Successfully bought product');
+      req.session.cart = null;
+      res.redirect('/');
+     });
   });
 });
 
